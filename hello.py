8fb09db308a88ba0281118584f2a56a08de56cbe
Karina Telles <karinac.telles@gmail.com>
2023-09-17 15:24:39 -0300

    Testando ifs

--- hello.py
+++ hello.py
@@ -1,7 +1,34 @@
 #Estudando Python
 """
-pssss o barulho da Python.
+Hello World multi idiomas
+
+O script exibe o idioma configurado no ambiente
+
+Tenha a variável LANG devidamente configurada. Exemplo:
+
+    export LANG=pt_BR
+
+    Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+    
 """
+__version__ = "0.0.1"
+__author__ = "Karina Cardoso Telles"
+__license__ = "Unlicense"
+
+current_language = "en_US"
+msg = "Hello, World"
+
+if current_language == "pt_BR":
+    msg = "Olá, mundo"
+    
+elif current_language == "it_IT":
+    msg = "Ciao, mondo!"
+
+
 print('karina'.upper())
 print(57+8)
-print('Hello, World!')
+print(msg)
