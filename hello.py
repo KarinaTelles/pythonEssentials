74cc58acf6b4526108aacc3a4fbcad501c490b56
Karina Telles <karinac.telles@gmail.com>
2023-09-19 12:43:22 -0300

    Informando sobre formas de dividir uma string para melhorar o entendimento do código

--- hello.py
+++ hello.py
@@ -20,6 +20,34 @@ __author__ = "Karina Cardoso Telles"
 __license__ = "Unlicense"
 
 import os
+"""
+O Python tem uma biblioteca que busca dados do sistema operacional. 
+Se for buscado algo como os.getenv("LANG") o resultado vai ser uma variável de ambiente 
+como essa vai ser retornado:  en_US.UTF-8 
+O objetivo era apenas buscar o idioma, então é possível trazer apenas os caracteres que se quer 
+ou fazer um split e retornar a primeira posição do array.
+
+***********************************************************************************************
+Exemplo buscando apenas os caracteres:
+(Trás apenas os 5 primeiros caracteres da string)
+
+os.getenv("LANG")[:5]
+
+***********************************************************************************************
+Exemplo usando o split e retornando apenas a posição zero do agora array de 2 posições:
+(A string foi "partida" onde tinha um ponto e cada metade virou uma posição no array)
+
+os.getenv("LANG").split(".")[0]
+
+***********************************************************************************************
+É uma boa prática definir um valor defaut caso não exista a variável de ambiente "LANG".
+Exemplos com valores default:
+
+os.getenv("LANG","en_US")[:5]
+os.getenv("LANG", "en_US").split(".")[0]
+
+**********************************************************************************************
+"""
 
 #current_language = "en_US"
 current_language = os.getenv("LANG","en_US")[:5]
